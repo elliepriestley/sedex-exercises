9287c78122b648d1731cf3a5cf6090d47f0cfdb0
elliepriestley <ellie.priestley@gmail.com>
2023-12-07 11:56:47 +0000

    Refactored CharacterWithLongestConsecutiveRepetition kata to remove first if statement, add comments, and make variable names more descriptive

--- src/main/kotlin/easy_exercises/CharacterWithLongestConsecutiveRepetition.kt
+++ src/main/kotlin/easy_exercises/CharacterWithLongestConsecutiveRepetition.kt
@@ -4,7 +4,8 @@ package easy_exercises
 *
 * For a given string s find the character c (or C) with longest consecutive repetition and return:
     Pair(c, l)
-    where l (or L) is the length of the repetition. If there are two or more characters with the same l return the first in order of appearance.
+    where l (or L) is the length of the repetition. If there are two or more characters with the same l
+    *  return the first in order of appearance.
 
 For empty string return:
     Pair(null, 0)
@@ -12,27 +13,26 @@ For empty string return:
 
 
 class CharacterWithLongestConsecutiveRepetition {
-    fun returnCharacter(string: String): Pair<Char?, Int> {
+    fun returnCharacter(inputString: String): Pair<Char?, Int> {
         var mostFrequentConsecutiveChar: Char? = null
         var mostFrequentConsecutiveCharCount: Int = 0
-        var counter = 0
+        var currentCount = 0
 
-        string.forEachIndexed { index, char ->
-            if (index == 0) {
-                mostFrequentConsecutiveChar = char
-                mostFrequentConsecutiveCharCount += 1
-                counter += 1
-            } else if (char == string[index - 1]) {
-                counter += 1
+        inputString.forEachIndexed { index, currentChar ->
+            // if it is the same as the one before
+            // input = abba
+            if (index  > 0 && currentChar == inputString[index - 1]) {
+                currentCount += 1
             } else {
-                counter = 1
+                currentCount = 1
             }
-            if (counter > mostFrequentConsecutiveCharCount) {
+            // if it has surpassed the previous most frequent
+            if (currentCount > mostFrequentConsecutiveCharCount) {
                 mostFrequentConsecutiveCharCount += 1
-                mostFrequentConsecutiveChar = char
+                mostFrequentConsecutiveChar = currentChar
             }
         }
-    return Pair(mostFrequentConsecutiveChar, mostFrequentConsecutiveCharCount)
+        return Pair(mostFrequentConsecutiveChar, mostFrequentConsecutiveCharCount)
     }
 }
 
